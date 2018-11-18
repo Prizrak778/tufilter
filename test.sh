79238fa3f01a3287d02d6ddfbd432eb92f02546c
Владимир <v.merkel778@gmail.com>
2018-11-18 20:14:39 +0700

    Добавлен README, в котором написано описание программы и параметры для неё

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #тест по блокировки всех сайтов
 ./tufilter --transport TCP --port 443 --filter Enable
-./tufilter --transport TCP --port 80 --filter Enable
+./tufilter --transport tcp --port 80 --filter Enable
 ./tufilter --transport udp --port 443 --filter Enable
-./tufilter --transport udp --port 80 --filter Enable
+./tufilter --transport uDp --port 80 --filter Enable
 #./tufilter --transport udp --port 80 --filter disable
-#./tufilter --transport udp --port 80 --filter disable
+#./tufilter --transport udp --ip 8.8.8.8 --port 80 --filter Enable
+#./tufilter --transport udp --ip 8.8.8.8 --filter Enable
 #./tufilter --show
